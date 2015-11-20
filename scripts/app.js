ab77cee3d1bf19cd2a3f655088f39091bb7ae028
mrmooby <scotthayward@gmail.com>
2015-11-20 09:36:25 -0500

    fully animated, clean-up code required

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
+	
+	// fade in each section on scroll
 
+
+	tiles = $('section').fadeTo(0, 0);
+
+	$(window).scroll(function(delay, opacity) {
+	    tiles.each(function(i) {
+	        elementBottom = $(this).offset().top + 300;
+	        windowBottom = $(window).scrollTop() + $(window).height() + 200;
+	        if (elementBottom < windowBottom) $(this).fadeTo(700, 1);
+	    });
+	});
 
 	// animate scroll to section when link is clicked
 
@@ -8,5 +20,8 @@ $(document).ready(function(){
 	        scrollTop: $( $(this).attr('href') ).offset().top}, 600);
 	    return false;
 	});
+
+
+
 
 });
