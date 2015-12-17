e3914c1ef463b7cb2ebfae4dd08d2dc92952c00a
mrmooby <scotthayward@gmail.com>
2015-12-17 08:58:00 -0500

    projects section still sucks, need to tweak responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,70 @@
 $(document).ready(function(){
 
-	// fade the first two sections of the page in on load
-	$('.on-page-load').css('display', 'none');
-	$('.on-page-load').fadeIn(750);
-
-	// fade in each remaining section on scroll
-
-	tiles = $('.faded').fadeTo(0, 0);
-
-	$(window).scroll(function(delay, opacity) {
-	    tiles.each(function(i) {
-	        elementBottom = $(this).offset().top + 400;
-	        windowBottom = $(window).scrollTop() + $(window).height() + 300;
-	        if (elementBottom < windowBottom) $(this).fadeTo(700, 1);
-	    });
-	});
-
-	// animate scroll to section when link is clicked
-
-	$('a').click(function(){
-	    $('html, body').animate({
-	        scrollTop: $( $(this).attr('href') ).offset().top}, 600);
-	    return false;
-	});
-
-	// animate menu on scroll
-
-	var windowPosition = $(window);
-	var currentPosition = windowPosition.scrollTop();
-	var goingUp = false;
-	var scrollPosition;
-	windowPosition.scroll(function () {
-	    scrollPosition = windowPosition.scrollTop();
-	    if (scrollPosition > currentPosition && !goingUp) {
-	        $('.nav').stop().slideToggle();
-	        goingUp = !goingUp;
-	        console.log(goingUp);
-	    } else if(scrollPosition < currentPosition && goingUp) {
-	        $('.nav').stop().slideToggle();
-	        goingUp = !goingUp;
+	var isMobile = {
+	    Android: function() {
+	        return navigator.userAgent.match(/Android/i);
+	    },
+	    BlackBerry: function() {
+	        return navigator.userAgent.match(/BlackBerry/i);
+	    },
+	    iOS: function() {
+	        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	    },
+	    Opera: function() {
+	        return navigator.userAgent.match(/Opera Mini/i);
+	    },
+	    Windows: function() {
+	        return navigator.userAgent.match(/IEMobile/i);
+	    },
+	    any: function() {
+	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
 	    }
-	    currentPosition = scrollPosition;
-	});
+	};
+
+    if(!isMobile.any()) {
+
+		// fade the first two sections of the page in on load
+		$('.on-page-load').css('display', 'none');
+		$('.on-page-load').fadeIn(750);
+
+		// fade in each remaining section on scroll
+
+		tiles = $('.faded').fadeTo(0, 0);
+
+		$(window).scroll(function(delay, opacity) {
+		    tiles.each(function(i) {
+		        elementBottom = $(this).offset().top + 400;
+		        windowBottom = $(window).scrollTop() + $(window).height() + 300;
+		        if (elementBottom < windowBottom) $(this).fadeTo(700, 1);
+		    });
+		});
+
+		// animate scroll to section when link is clicked
+
+		$('a').click(function(){
+		    $('html, body').animate({
+		        scrollTop: $( $(this).attr('href') ).offset().top}, 600);
+		    return false;
+		});
+
+		// animate menu on scroll
+
+		var windowPosition = $(window);
+		var currentPosition = windowPosition.scrollTop();
+		var goingUp = false;
+		var scrollPosition;
+		windowPosition.scroll(function () {
+		    scrollPosition = windowPosition.scrollTop();
+		    if (scrollPosition > currentPosition && !goingUp) {
+		        $('.nav').stop().slideToggle();
+		        goingUp = !goingUp;
+		        console.log(goingUp);
+		    } else if(scrollPosition < currentPosition && goingUp) {
+		        $('.nav').stop().slideToggle();
+		        goingUp = !goingUp;
+		    }
+		    currentPosition = scrollPosition;
+		});
+	}
 
 });
