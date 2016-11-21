e618e899e4af24b15a6170dfdd5660bace75300d
Scott Hayward <scotthayward@gmail.com>
2016-11-21 14:42:22 -0500

    rev 3 much better layout and cleaner code

--- scripts/app.js
+++ scripts/app.js
@@ -1,78 +1,25 @@
 $(document).ready(function(){
 	
-	// adjust rendering for devices
+	// start adjust rendering for devices
+	
 	var viewPortScale = 1 / window.devicePixelRatio;
 	$('#viewport').attr('content', 'user-scalable=no, initial-scale='+viewPortScale+', width=device-width');
+	
+	// end adjust rendering for devices
 
-	// determine if content is being viewed on a mobile device
-	var isMobile = {
-	    Android: function() {
-	        return navigator.userAgent.match(/Android/i);
-	    },
-	    BlackBerry: function() {
-	        return navigator.userAgent.match(/BlackBerry/i);
-	    },
-	    iOS: function() {
-	        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-	    },
-	    Opera: function() {
-	        return navigator.userAgent.match(/Opera Mini/i);
-	    },
-	    Windows: function() {
-	        return navigator.userAgent.match(/IEMobile/i);
-	    },
-	    any: function() {
-	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-	    }
-	};
 
-	// Autohide menu on scroll
-		var windowPosition = $(window);
-		var currentPosition = windowPosition.scrollTop();
-		var goingUp = false;
-		var scrollPosition;
-		windowPosition.scroll(function () {
-		    scrollPosition = windowPosition.scrollTop();
-		    if (scrollPosition > currentPosition && !goingUp) {
-		        $('nav').stop().fadeToggle();
-		        goingUp = !goingUp;
-		        console.log(goingUp);
-		    } else if(scrollPosition < currentPosition && goingUp) {
-		        $('nav').stop().fadeToggle();
-		        goingUp = !goingUp;
-		    }
-		    currentPosition = scrollPosition;
-		});
 
-	// if the device is NOT mobile, allow animations
-    if(!isMobile.any()) { 
+	// start animate scroll to section when link is clicked
 
-		// fade the first two sections of the page in on load
-		$('.on-page-load').css('display', 'none');
-		$('.on-page-load').fadeIn(750);
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $(this).attr('href') ).offset().top}, 600);
+	    return false;
+	});
 
-		// fade in each remaining section on scroll
+	// end animate on scroll to section when link is clicked
 
-		tiles = $('.faded').fadeTo(0, 0);
-
-		$(window).scroll(function(delay, opacity) {
-		    tiles.each(function(i) {
-		        elementBottom = $(this).offset().top + 400;
-		        windowBottom = $(window).scrollTop() + $(window).height() + 300;
-		        if (elementBottom < windowBottom) $(this).fadeTo(700, 1);
-		    });
-		});
-
-		// animate scroll to section when link is clicked
-
-		$('a').click(function(){
-		    $('html, body').animate({
-		        scrollTop: $( $(this).attr('href') ).offset().top}, 600);
-		    return false;
-		});
-	}
-
-	// contact form
+	// start contact form
 
 	$('#slickform').on('submit', function(e) {
         e.preventDefault();
@@ -100,5 +47,7 @@ $(document).ready(function(){
         });     
         
     });
+
+    // end contact form
 
 });
